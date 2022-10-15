05b83431839019213ad11bd68b774d7c061e60f5
flawl1ss <flawl1ss.haha@gmail.com>
2022-10-15 18:31:58 +0300

    dz

---
+++ main.js
@@ -0,0 +1,103 @@
+// const table = document.createElement('table')
+// document.body.append(table)
+// let num = 0
+// for (let i = 1; i <= 10; i++) {
+//     const tr = document.createElement('tr')
+//     table.append(tr)
+//     for (let j = 0; j < 10; j++) {
+//         const td = document.createElement('td')
+//         num += 1
+//         td.textContent = num
+//         tr.append(td)
+//     }
+// }
+
+
+///////////////////////// ДЗ //////////////////////
+
+
+// let one = prompt('Введите первое число')
+// let two = prompt('Введите второе число')
+// function compare(a,b) {
+//     if(a === b) {
+//         return alert(`числа ${a} и ${b} равны`)
+//     }else if(a > b){
+//         return alert(`число ${a} больше числа ${b}`)
+//     }else if(a < b){
+//         return alert(`число ${a} меньше числа ${b} `)
+//     }
+// }
+// compare(one, two)
+
+
+
+
+// let one = prompt('Введите первое число')
+// let two = prompt('Введите второе число')
+// function analizeNumbers(n1, n2) {
+                                                        // не сделал
+// }
+
+
+
+
+
+// const amount = prompt('Введите количество игроков')
+// const name = prompt('Введите имя игрока')
+// const size = prompt('Введите размер обуви игрока')
+// const team = {}                                                  // не получилось
+// function getShoes(amount, name, size) {
+//     for(let i = 0; i <= amount; i++) {
+//         team.name = size
+//     }
+//     return alert = team
+// }
+
+
+
+
+// const country = prompt('Введите страну(USA, Germany, Japan)')
+// function getCar(country) {
+//     if(country === 'USA') {
+//         alert('Tesla')
+//     }else if(country === 'Germany') {
+//         alert('Mercedes')
+//     }else if(country === 'Japan') {
+//         alert('Toyota')
+//     }
+// }
+// getCar(country)
+
+
+
+
+// const height = prompt('Введите высоту')
+// const width = prompt('Введите ширину')
+// function getArea(height, width) {
+//     if(height === 0) {
+//         console.log(null)
+//     }else if(width === 0) {             // с отменой не получилось
+//         console.log(null)
+//     }else {
+//         return console.log(height * width)
+//     }
+// }
+// getArea(height, width)
+
+
+
+
+
+
+// function checkPass() {
+//     const password = prompt('Введите пароль')
+//     for(let i = 0; i <= 3; i++){
+//         if(password === 'admin') {
+//             return alert('Привет, админ!')                     // Не до конца сделал
+//         }else {
+//             alert('Неверный пароль')
+//         }
+//     }
+//     return 'Мы вас не знаем!'
+// }
+// checkPass()
